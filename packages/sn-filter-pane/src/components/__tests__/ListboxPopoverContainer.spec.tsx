f25e37e5012c7ebb8a393a069a5e3fdfcc94351d
Daniel Sjöstrand <99665802+DanielS-Qlik@users.noreply.github.com>
2022-11-28 16:40:40 +0100

    test: add unit test to popover

---
+++ packages/sn-filter-pane/src/components/__tests__/ListboxPopoverContainer.spec.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import { IListboxResource } from '../../hooks/types';
+import { ListboxPopoverContainer } from '../ListboxPopoverContainer';
+
+const resource = {
+  id: '123',
+  layout: {
+    title: 'Foo',
+    qListObject: {
+      qDimensionInfo: {
+        qStateCounts: {
+          qSelected: 10,
+          qLocked: 10,
+          qOption: 10,
+          qAlternative: 10,
+        },
+        qCardinal: 40,
+      },
+    },
+  },
+};
+const resources: IListboxResource[] = [...Array(4)].map(() => ({ ...resource as IListboxResource }));
+
+describe('ListboxPopoverContainer render', () => {
+  it('should render FoldedListbox with fieldname when one resource is provided', () => {
+    const { queryByText } = render(
+      <ListboxPopoverContainer resources={[resource as IListboxResource]} />,
+    );
+    expect(queryByText('Foo')).toBeTruthy();
+  });
+
+  it('should render ExpandButton with number of hidden Listboxes when multiple resources are provided', () => {
+    const { queryByText } = render(
+      <ListboxPopoverContainer resources={resources as IListboxResource[]} />,
+    );
+    expect(queryByText('4')).toBeTruthy();
+  });
+
+  it('should render 4 "Foo" items when clicked', () => {
+    const { queryByText, getAllByText } = render(
+      <ListboxPopoverContainer resources={resources as IListboxResource[]} />,
+    );
+    fireEvent.click(queryByText('4') as Element);
+    const items = getAllByText('Foo');
+    expect(items.length).toBe(4);
+  });
+});
