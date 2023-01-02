295117aa6f46fbac600638daa87a8d4ca0d22440
Johan Lahti <johanlahti@gmail.com>
2023-01-02 11:03:47 +0100

    refactor: align listbox properties for fp and lb

--- packages/sn-filter-pane/src/qae/filter-pane-properties.ts
+++ packages/sn-filter-pane/src/qae/filter-pane-properties.ts
@@ -3,42 +3,9 @@ const properties = {
   title: '',
   subtitle: '',
   footnote: '',
-  histogram: false,
-  checkboxes: false,
-  layoutOptions: {
-    dense: false,
-    dataLayout: 'singleColumn',
-    layoutOrder: 'row',
-    maxVisibleRows: {
-      auto: true,
-      maxRows: 3,
-    },
-    maxVisibleColumns: {
-      auto: true,
-      maxColumns: 3,
-    },
-  },
   qChildListDef: {
     qDef: {
-      qListObjectDef: {
-        qLibraryId: undefined,
-        qFrequencyMode: 'N',
-        qDef: {
-          cId: '',
-          autoSort: true,
-          qFieldDefs: [],
-          textAlign: {
-            auto: true,
-            align: 'left',
-          },
-          qSortCriterias: [],
-          qChildListDef: {
-            qData: {
-              info: '/qInfo',
-            },
-          },
-        },
-      },
+      // Listbox (default) properties (listbox-properties.ts) go here… See "add"-function in data.ts
     },
   },
 };
--- packages/sn-listbox/src/object-properties.ts
+++ packages/sn-listbox/src/object-properties.ts
@@ -3,11 +3,26 @@ const properties = {
     qShowAlternatives: true,
     frequencyEnabled: false,
     qFrequencyMode: 'N',
-    // qInitialDataFetch: [],
     qDef: {
       autoSort: true,
       textAlign: { auto: true, align: 'left' },
+      qSortCriterias: [
+        {
+          qSortByState: 1,
+          qSortByAscii: 1,
+          qSortByNumeric: 1,
+          qSortByLoadOrder: 1,
+        },
+      ],
     },
+    qInitialDataFetch: [
+      {
+        qTop: 0,
+        qLeft: 0,
+        qWidth: 0,
+        qHeight: 0,
+      },
+    ],
   },
   checkboxes: false,
   histogram: false, // note that histogram will not show as long as qFrequencyMode == 'N'
@@ -25,8 +40,18 @@ const properties = {
     },
   },
   title: '',
+  showTitles: false,
   subtitle: '',
   footnote: '',
+  disableNavMenu: false,
+  showDetails: false,
+  showDetailsExpression: false,
+  qStateName: '',
+  qInfo: {
+    qType: 'listbox',
+  },
+  // visualization: 'listbox',
+  targets: [],
 };
 
 export default properties;
