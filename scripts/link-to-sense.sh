57db0e30f954c2bd37dfcfc16f8dea8ccd073f34
Johan Lahti <johanlahti@gmail.com>
2023-01-02 14:55:52 +0100

    chore: use yarn

--- scripts/link-to-sense.sh
+++ scripts/link-to-sense.sh
@@ -2,7 +2,7 @@
 GREEN="\033[1;32m"
 NOCOLOR="\033[0m"
 if [ ! -f ~/Qlik/Sense/Extensions/sn-filter-pane-ext/dist/sn-filter-pane.js ]; then
-  echo "Run: ${GREEN}npm run sense${NOCOLOR} then copy ./sn-filter-pane-ext to ~/Qlik/Sense/Extensions/ then try this again"
+  echo "Run: ${GREEN}yarn run sense${NOCOLOR} then copy ./sn-filter-pane-ext to ~/Qlik/Sense/Extensions/ then try this again"
   exit
 else
   if [ -f ~/Qlik/Sense/Extensions/sn-filter-pane-ext/dist/sn-filter-pane.js.map ]; then
@@ -10,7 +10,7 @@ else
   fi
   rm ~/Qlik/Sense/Extensions/sn-filter-pane-ext/dist/sn-filter-pane.js
 fi
-npm run build
+yarn run build
 ln ./dist/sn-filter-pane.js ~/Qlik/Sense/Extensions/sn-filter-pane-ext/dist/sn-filter-pane.js
 ln ./dist/sn-filter-pane.js.map ~/Qlik/Sense/Extensions/sn-filter-pane-ext/dist/sn-filter-pane.js.map
 echo "${GREEN} Press Y/y to refresh your current Chrome tab when rebuilding: ${NOCOLOR}"
@@ -20,6 +20,6 @@ if [ "$CHOICE" = 'y' ] || [ "$CHOICE" = 'Y'  ]; then
   fswatch -o ./dist | xargs -n1 -I {} osascript -e 'tell application "Google Chrome" to tell the active tab of its first window to reload' & pid=$!
   trap "kill $pid" SIGINT
 else
-    echo "'$CHOICE' not 'Y' or 'y'. Just running npm run build:watch"
+    echo "'$CHOICE' not 'Y' or 'y'. Just running yarn run build:watch"
 fi
-npm run build:watch
+yarn run build:watch
