938a1ba232522ea83045166404099c5e8cc7a719
costa <costantino.marullo@qlik.com>
2023-01-13 13:19:02 +0100

    fix: import properties

--- packages/sn-filter-pane/src/ext/conversion/index.ts
+++ packages/sn-filter-pane/src/ext/conversion/index.ts
@@ -9,27 +9,13 @@ interface ExportFormat {
 
   interface PropTree {
     qChildren: unknown[];
-    qProperty: EngineAPI.IGenericHyperCubeProperties;
   }
 
 export function createImportProperties(env: IEnv) {
-  const dataDefinition = getData(env);
-
-  return (
-    exportFormat: ExportFormat,
-    initialProperties: EngineAPI.IGenericHyperCubeProperties,
-  ): PropTree => {
-    const propTree = conversion.hypercube.importProperties({
-      exportFormat,
-      initialProperties,
-      dataDefinition: dataDefinition.targets[0],
-    });
-
-    return propTree;
-  };
+  // TODO: add properties for unquarantine
 }
 
-export function exportProperties(propertyTree: PropTree, hyperCubePath: string): ExportFormat {
+export function exportProperties(propertyTree: PropTree): ExportFormat {
   const expFormat = conversion.hypercube.exportProperties({
     propertyTree,
   });
