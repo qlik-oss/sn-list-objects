090ae5d65c1e04ae89df270cdcaa31d7a46f052a
Quan Ho <quan.ho@qlik.com>
2023-03-06 10:48:11 +0100

    fix: fix path for counting the number of dimensions (#106)

--- packages/sn-filter-pane/src/qae/data.ts
+++ packages/sn-filter-pane/src/qae/data.ts
@@ -11,7 +11,7 @@ export default function getData(env: IEnv) {
     measures: { min: 0, max: 0 },
     targets: [
       {
-        path: '/notExisting/qListObjectDef',
+        path: '/qChildListDef',
         measures: { min: 0, max: 0 },
         dimensions: {
           min: 1,
