f33d07ff0af17d6e680f78899ca9636c68c6d1fa
Quan Ho <quan.ho@qlik.com>
2023-03-16 13:49:57 +0100

    fix: fix setting focus on keyboard navigation (#150)

--- packages/sn-filter-pane/src/components/ListboxGrid/ListboxGrid.tsx
+++ packages/sn-filter-pane/src/components/ListboxGrid/ListboxGrid.tsx
@@ -23,6 +23,7 @@ import ConditionalWrapper from './ConditionalWrapper';
 import type { IStores } from '../../store';
 import { ListboxPopoverContainer } from '../ListboxPopoverContainer';
 import useHandleActive, { ActiveOnly } from './use-handle-active';
+import KEYS from './keys';
 
 // TODO: Remove
 const Resizable = styled(ResizableBox)(() => ({
@@ -31,7 +32,7 @@ const Resizable = styled(ResizableBox)(() => ({
 
 function ListboxGrid({ stores }: { stores: IStores }) {
   const { store, useResourceStore } = stores;
-  const { sense, selections } = store.getState();
+  const { sense, selections, keyboard } = store.getState();
   const resources = useResourceStore((state) => state.resources);
 
   const gridRef = useRef<HTMLDivElement>();
@@ -53,6 +54,46 @@ function ListboxGrid({ stores }: { stores: IStores }) {
     setColumns(expandedAndCollapsedColumns);
   }, [resources]);
 
+  const preventDefaultBehavior = (event: React.KeyboardEvent | MouseEvent | React.MouseEvent<HTMLLIElement>) => {
+    event.stopPropagation();
+    event.preventDefault();
+  };
+
+  const findIndex = (element: EventTarget, nodeList: NodeList) => {
+    for (let i = 0; i < nodeList.length; i++) {
+      if (element === nodeList.item(i)) return i;
+    }
+    return -1;
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === KEYS.ESC && keyboard?.enabled) {
+      // @ts-ignore
+      if (event.target.classList.contains('listbox-container')) {
+        // Focus currently on a listbox
+        preventDefaultBehavior(event);
+        // @ts-ignore
+        keyboard.blur?.(true);
+      }
+    } else if (event.key === KEYS.LEFT || event.key === KEYS.RIGHT) {
+      let elementToFocus;
+      const listboxList = gridRef?.current?.querySelectorAll && gridRef?.current?.querySelectorAll('.listbox-container');
+      if (listboxList?.length) {
+        const activeIndex = findIndex(event.target, listboxList);
+        if (activeIndex < 0) {
+          elementToFocus = listboxList.item(0);
+        } else if (!(listboxList.length === 1 || (activeIndex === 0 && event.key === KEYS.LEFT) || (activeIndex === listboxList.length - 1 && event.key === KEYS.RIGHT))) {
+          const nextIndex = event.key === KEYS.LEFT ? activeIndex - 1 : activeIndex + 1;
+          elementToFocus = listboxList.item(nextIndex);
+        }
+        if (elementToFocus) {
+          (elementToFocus as HTMLElement).focus();
+        }
+      }
+      preventDefaultBehavior(event);
+    }
+  };
+
   const isRtl = options.direction === 'rtl';
 
   useEffect(() => {
@@ -60,6 +101,17 @@ function ListboxGrid({ stores }: { stores: IStores }) {
       handleResize();
     }
   }, [resources]);
+
+  useEffect(() => {
+    const firstChild = gridRef?.current?.querySelector && gridRef?.current?.querySelector('.listbox-container') as HTMLDivElement;
+    if (keyboard?.active) {
+      firstChild?.setAttribute('tabIndex', '0');
+      firstChild?.focus();
+    } else {
+      firstChild?.setAttribute('tabIndex', '-1');
+      firstChild?.blur();
+    }
+  }, [keyboard]);
 
   const handleActive = useHandleActive(isInSense, selections as stardust.ObjectSelections & ActiveOnly);
   const dHandleResize = debounce(handleResize, isInSense ? 0 : 50);
@@ -72,7 +124,7 @@ function ListboxGrid({ stores }: { stores: IStores }) {
       >
         <>
           <ElementResizeListener onResize={dHandleResize} />
-          <Grid container sx={{ flexDirection: isRtl ? 'row-reverse' : 'row' }} columns={columns?.length} ref={gridRef as unknown as () => HTMLDivElement} spacing={0} height='100%'>
+          <Grid container onKeyDown={handleKeyDown} sx={{ flexDirection: isRtl ? 'row-reverse' : 'row' }} columns={columns?.length} ref={gridRef as unknown as () => HTMLDivElement} spacing={0} height='100%'>
 
             {!!columns?.length && columns?.map((column: IColumn, i: number) => (
               <ColumnGrid key={i} widthPercent={100 / columns.length}>
