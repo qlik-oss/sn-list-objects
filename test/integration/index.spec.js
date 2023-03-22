b9f043ddddbce915d2de445a8e903c82e47f4c2e
Per Nylander <per.nylander@qlik.com>
2023-03-22 08:26:35 +0100

    test: integration tests

---
+++ test/integration/index.spec.js
@@ -0,0 +1,64 @@
+import path from 'path';
+import serve from '@nebula.js/cli-serve';
+import { test, expect, chromium } from '@playwright/test';
+import createNebulaRoutes from '../utils/routes';
+import { checkScreenshot, getTooltipContent } from '../utils/shared';
+
+test.describe('sn-filter-pane: ui integration tests to test visual bugs', () => {
+  let s;
+  let route;
+
+  test.beforeAll(async () => {
+    s = await serve({
+      entry: path.resolve(
+        __dirname,
+        '../../packages/sn-filter-pane/dist/sn-filter-pane.js'
+      ),
+      type: 'sn-filter-pane',
+      open: false,
+      build: false,
+      themes: [],
+      flags: {},
+      fixturePath: 'test/integration/__fixtures__',
+    });
+
+    route = createNebulaRoutes(s.url);
+  });
+
+  test.afterAll(async () => {
+    s.close();
+  });
+
+  test.describe('Interaction', () => {
+    test('Open search', async () => {
+      const renderUrl = await route.renderFixture('multi_scenario_3.fix.js');
+      const browser = await chromium.launch();
+      const context = await browser.newContext();
+      const page = await context.newPage();
+      await page.goto(renderUrl, { waitUntil: 'networkidle' });
+      await page.locator('[data-testid="search-toggle-btn"]').nth(1).click();
+      await page.waitForTimeout(500);
+      await checkScreenshot(
+        '.njs-viz[data-render-count="1"]',
+        page,
+        'open_search.png'
+      );
+    });
+    test('Open collapsed filterpane', async () => {
+      const renderUrl = await route.renderFixture('multi_scenario_3.fix.js');
+      const browser = await chromium.launch();
+      const context = await browser.newContext();
+      const page = await context.newPage();
+      await page.goto(renderUrl, { waitUntil: 'networkidle' });
+      await (
+        await page.waitForSelector('[data-testid="collapsed-title-City"]')
+      ).click();
+      await page.waitForTimeout(500);
+      await checkScreenshot(
+        '.njs-viz[data-render-count="1"]',
+        page,
+        'open_collapsed_filterpane.png'
+      );
+    });
+  });
+});
