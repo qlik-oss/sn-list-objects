f4749c53346ea42d3bc9eba09c4f863591042e45
Tobias Linsefors <tobias.linsefors@qlik.com>
2023-03-23 10:04:22 +0100

    feat: add sorting in simple edit mode

--- packages/sn-filter-pane/src/ext/property-panel/index.ts
+++ packages/sn-filter-pane/src/ext/property-panel/index.ts
@@ -1,6 +1,7 @@
 import settings from './settings';
 import { IEnv } from '../../types/types';
 import getData from './data';
+import simpleSorting from './simple-sorting';
 
 export default function pp(env: IEnv) {
   const out = {
@@ -8,6 +9,7 @@ export default function pp(env: IEnv) {
     component: 'accordion',
     items: {
       data: getData(env),
+      simpleSorting,
       settings,
     },
   };
