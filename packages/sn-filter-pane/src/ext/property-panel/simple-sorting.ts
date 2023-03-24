ca26b65d672ec8e49e9e8b11dc253e6c397ecd50
Tobias Linsefors <T-Wizard@users.noreply.github.com>
2023-03-24 09:56:42 +0100

    feat: add sorting in simple edit mode (#211)

--- packages/sn-filter-pane/src/ext/property-panel/simple-sorting.ts
+++ packages/sn-filter-pane/src/ext/property-panel/simple-sorting.ts
@@ -1,28 +1,10 @@
 const simpleSorting = {
-  // component: SimpleSorting,
-  // classification: {
-  //   section: 'sorting',
-  //   tags: ['simple'],
-  //   exclusive: true,
-  // },
-  // reset: (data, args) => {
-  //   const { layout } = args;
-  //   const childIds = layout.qChildList.qItems.map((item) => item.qInfo.qId);
-
-  //   DimensionModel.getListLayout().then(() => {
-  //     getListboxInfos(args.model.app, childIds).then((listboxInfos) => {
-  //       const defaultSortCriterias = {
-  //         qSortByState: 1,
-  //         qSortByAscii: 1,
-  //         qSortByNumeric: 1,
-  //       };
-  //       Object.keys(listboxInfos).forEach((id) => {
-  //         util.setValue(listboxInfos[id].properties, 'qListObjectDef.qDef.qSortCriterias.0', defaultSortCriterias);
-  //         listboxInfos[id].model.setProperties(listboxInfos[id].properties);
-  //       });
-  //     });
-  //   });
-  // },
+  component: 'filter-pane-sorting',
+  classification: {
+    section: 'sorting',
+    tags: ['simple'],
+    exclusive: true,
+  },
 };
 
 export default simpleSorting;
