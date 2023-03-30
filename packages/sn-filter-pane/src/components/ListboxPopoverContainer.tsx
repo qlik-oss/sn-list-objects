ac63c10ed1f4a09e28a34af1aea0bd346d1d4144
Purwa Shrivastava <atq@qlik.com>
2023-03-30 16:09:00 +0200

    fix: show selection toolbar when listbox opens in popover

--- packages/sn-filter-pane/src/components/ListboxPopoverContainer.tsx
+++ packages/sn-filter-pane/src/components/ListboxPopoverContainer.tsx
@@ -96,7 +96,7 @@ export const ListboxPopoverContainer = ({ resources, stores }: FoldedPopoverProp
         marginThreshold= {isSmallDevice ? 0 : 16}
       >
         {(selectedResource || isSingle)
-          ? <ListboxContainer layout={selectedResource?.layout ?? resources[0].layout} stores={stores} closePopover={ closePopover } />
+          ? <ListboxContainer layout={selectedResource?.layout ?? resources[0].layout} stores={stores} closePopover={closePopover} isCollapsed={true} />
           : <Grid container direction='column' spacing={1} padding='8px'>
             {!!resources?.length && resources?.map((resource) => (
               <Grid item key={resource.id}>
