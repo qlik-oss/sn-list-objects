33838bd92b5afe5e9a7a1b3d093147937f5c62f1
Daniel Sjöstrand <99665802+DanielS-Qlik@users.noreply.github.com>
2023-04-18 10:56:13 +0200

    refactor: move filter-pane-properties to object-definition. add api spec

---
+++ packages/sn-filter-pane/api-spec/props.conf.js
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
+const pkg = require(path.resolve(__dirname, '../package.json')); // eslint-disable-line
+
+module.exports = {
+  fromJsdoc: {
+    glob: ['./src/qae/object-definition.js'],
+    package: path.resolve(__dirname, '../package.json'),
+    api: {
+      stability: 'experimental',
+      properties: {
+        'x-qlik-visibility': 'public',
+      },
+      visibility: 'public',
+      name: `${pkg.name}:properties`,
+      version: pkg.version,
+      description: 'Filter pane generic object definition',
+    },
+    output: {
+      sort: {
+        alpha: true,
+      },
+      file: './api-spec/spec.json',
+    },
+    parse: {
+      types: {
+        StringExpression: {},
+        'EngineAPI.IGenericListProperties': {},
+        'EngineAPI.IListObjectDef': {},
+      },
+    },
+  },
+};
---
+++ packages/sn-filter-pane/src/qae/object-definition.js
@@ -0,0 +1,82 @@
+/**
+ * @namespace properties
+ * @entry
+ */
+const objectDefinition = () => {
+  /**
+   * @lends properties
+   */
+  const definition = {
+    /**
+     * Current version of this generic object definition
+     * @type {string}
+     * @default
+     */
+    version: process.env.PACKAGE_VERSION,
+    /**
+     * Show visualization details toggle
+     * @type {boolean=}
+     * @default
+     */
+    showDetails: true,
+    /**
+     * Show title for the visualization.
+     * @type {boolean=}
+     * @default
+     */
+    showTitles: false,
+    /**
+     * Visualization title.
+     * @type {(string|StringExpression)=}
+     * @default
+     */
+    title: '',
+    /**
+     * Visualization subtitle.
+     * @type {(string|StringExpression)=}
+     * @default
+     */
+    subtitle: '',
+    /**
+     * Visualization footnote.
+     * @type {(string|StringExpression)=}
+     * @default
+     */
+    footnote: '',
+    /**
+     * Child object definition
+     * @type {ChildListDef}
+     * @default {qDef:{qListObjectDef:{}}}
+     */
+    qChildListDef: {
+      qDef: {
+        qListObjectDef: {},
+      },
+    },
+  };
+
+  return definition;
+};
+
+export default objectDefinition;
+
+/**
+ * @name ChildListDef
+ * @type object
+ * @property {IGenericListProperties} qDef
+ */
+
+/**
+ * Extends `IGenericListProperties`, see Engine API: `IGenericListProperties`.
+ * @name IGenericListProperties
+ * @extends EngineAPI.IGenericListProperties
+ * @type object
+ * @property {IListObjectDef} qListObjectDef
+ */
+
+/**
+ * Extends `IListObjectDef`, see Engine API: `IListObjectDef`.
+ * @name IListObjectDef
+ * @type object
+ * @description See {@link https://qlik.dev/apis/json-rpc/qix/schemas#%23%2Fdefinitions%2Fschemas%2Fentries%2FListObjectDef}
+ */
