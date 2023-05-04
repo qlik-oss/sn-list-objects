f084512bd13f0b7f248cddd24096816d295efdfd
Daniel Sjöstrand <99665802+DanielS-Qlik@users.noreply.github.com>
2023-05-04 14:08:05 +0200

    feat: add example mashup

---
+++ packages/sn-filter-pane/examples/dev-mashup/src/interfaces.ts
@@ -0,0 +1,19 @@
+export interface GetQCSHeadersParams {
+  webIntegrationId: string,
+  url: string,
+}
+
+export interface ConnectParams extends GetQCSHeadersParams {
+  appId: string,
+}
+
+export interface Headers {
+  [key: string]: string | undefined,
+}
+
+export interface GetEnigmaApp {
+  host: string,
+  appId: string,
+  headers?: Headers,
+  isLocalhost: boolean,
+}
---
+++ packages/sn-filter-pane/examples/dev-mashup/src/main.ts
@@ -0,0 +1,53 @@
+import embed from './configure';
+import connect from './connect';
+
+async function run() {
+  const app = await connect({
+    url: 'http://localhost:9076', // 'xxxx.us.qlik.com' or 'http://localhost:9076'
+    webIntegrationId: '<Qlik web integration id>', // 'xxx-xxxxxxx-xxxxxxxx',
+    appId: 'apps/filter-pane2.qvf', // 'xxxx-xxx-xxx-xxx-xxxxxxx' or 'apps/Ctrl-00.qvf',
+  });
+
+  const toolbarElement = document.querySelector('.toolbar') as HTMLElement;
+  const filterPaneOptionsElement = document.querySelector('.fpOptions') as HTMLElement;
+  const filterPaneResizeElement = document.querySelector('.fpResize') as HTMLElement;
+
+  if (!toolbarElement || !filterPaneOptionsElement || !app) {
+    console.error('Element missing');
+    return;
+  }
+
+  const nuked = embed(app);
+
+  (await nuked.selections()).mount(toolbarElement);
+
+  // create a session object
+  nuked.render({
+    element: filterPaneOptionsElement,
+    type: 'filterpane',
+    fields: [
+      {
+        // @ts-ignore
+        qListObjectDef: {
+          qDef: { qFieldDefs: ['Dim1'] },
+          qFrequencyMode: 'P',
+        },
+        checkboxes: false,
+        histogram: true,
+        layoutOptions: {
+          dense: true,
+          dataLayout: 'singleColumn',
+          layoutOrder: 'row',
+        },
+      },
+    ],
+  });
+
+  nuked.render({
+    element: filterPaneResizeElement,
+    type: 'filterpane',
+    fields: ['Dim1', 'Dim2', 'Dim3'],
+  });
+}
+
+run();
