cdf3e838ac2eb82593aba4737edd763ce51b1c59
Daniel Sjöstrand <99665802+DanielS-Qlik@users.noreply.github.com>
2023-06-08 12:58:42 +0200

    test: add unit test

---
+++ packages/sn-filter-pane/src/components/FoldedListbox/__tests__/get-sizes.spec.ts
@@ -0,0 +1,40 @@
+import { ISize } from '../../ListboxGrid/interfaces';
+import { POPOVER_CONTAINER_PADDING } from '../../ListboxPopoverContainer';
+import getSizes from '../get-sizes';
+
+describe('getSizes', () => {
+  it.only('should return correct sizes for narrow-small container', () => {
+    const containerSize = {
+      height: 20,
+    };
+
+    const sizes = getSizes(containerSize as ISize);
+
+    expect(sizes.gridHeight).toBe(containerSize.height - POPOVER_CONTAINER_PADDING - 1);
+    expect(sizes.narrowSmall).toBe(true);
+    expect(sizes.narrowLarge).toBe(false);
+  });
+
+  it('should return correct sizes for narrow-large container', () => {
+    const containerSize = {
+      height: 24,
+    };
+
+    const sizes = getSizes(containerSize as ISize);
+
+    expect(sizes.gridHeight).toBe(containerSize.height - POPOVER_CONTAINER_PADDING - 1);
+    expect(sizes.narrowSmall).toBe(false);
+    expect(sizes.narrowLarge).toBe(true);
+  });
+
+  it('should return false for non-narrow container', () => {
+    const containerSize = {
+      height: 25,
+    };
+
+    const sizes = getSizes(containerSize as ISize);
+
+    expect(sizes.narrowSmall).toBe(false);
+    expect(sizes.narrowLarge).toBe(false);
+  });
+});
