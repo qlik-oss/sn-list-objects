bc9a1a4e880382d2f0e236fe09c5cdbea4061f0e
Daniel Sjöstrand <99665802+DanielS-Qlik@users.noreply.github.com>
2023-06-12 14:42:16 +0200

    fix: adapt expand button to narrow container (#320)

--- packages/sn-filter-pane/src/components/ExpandButton.tsx
+++ packages/sn-filter-pane/src/components/ExpandButton.tsx
@@ -1,8 +1,9 @@
-import { Button, styled } from '@mui/material';
+import { Button, Grid, styled } from '@mui/material';
 import React from 'react';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import { BUTTON_HEIGHT } from './ListboxGrid/distribute-resources';
 import type { IStores } from '../store';
+import POPOVER_CONTAINER_PADDING from './FoldedListbox/constants';
 
 export interface FoldedListboxProps {
   onClick?: (event: { event: React.MouseEvent<HTMLButtonElement> }) => void;
@@ -17,7 +18,7 @@ export interface IconProps {
 export const ExpandButton = ({
   onClick, opened, stores,
 }: FoldedListboxProps) => {
-  const { constraints, stardustTheme } = stores.store.getState();
+  const { constraints, stardustTheme, containerSize } = stores.store.getState();
 
   const ExpandIcon = styled(ExpandMore)<IconProps>(({ open }) => ({
     color: '#555555',
@@ -25,17 +26,27 @@ export const ExpandButton = ({
     transform: open ? 'rotate(180deg)' : 'rotate(0deg)',
   }));
 
-  const StyledButton = styled(Button)(() => ({
-    backgroundColor: stardustTheme?.getStyle('object', '', 'listBox.backgroundColor') ?? '#FFFFFF',
-    width: '100%',
-    height: BUTTON_HEIGHT,
-    border: '1px solid #B3B3B3',
-    borderRadius: '3px',
-    justifyContent: 'end',
+  const StyledButton = styled(Button)(() => {
+    let height = BUTTON_HEIGHT;
+    if (containerSize?.height && containerSize?.height < BUTTON_HEIGHT + POPOVER_CONTAINER_PADDING) {
+      height = containerSize.height - POPOVER_CONTAINER_PADDING - 1;
+    }
+    return {
+      backgroundColor: stardustTheme?.getStyle('object', '', 'listBox.backgroundColor') ?? '#FFFFFF',
+      width: '100%',
+      height,
+      border: '1px solid #B3B3B3',
+      borderRadius: '3px',
+      justifyContent: 'end',
+    };
+  });
+
+  const StyledDiv = styled('div')(() => ({
+    display: 'flex',
   }));
 
   return (
-    <>
+    <StyledDiv>
       <StyledButton
         onClick={(event) => onClick?.({ event })}
         disableRipple
@@ -44,6 +55,6 @@ export const ExpandButton = ({
       >
         <ExpandIcon open={opened}></ExpandIcon>
       </StyledButton>
-    </>
+    </StyledDiv>
   );
 };
