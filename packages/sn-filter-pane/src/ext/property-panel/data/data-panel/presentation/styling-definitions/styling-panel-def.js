ee00a4356c5e5bb7e532c5a3aabcf42a75b57ac3
costa <costantino.marullo@qlik.com>
2023-07-06 10:39:14 +0200

    feat: add styling panel (filterpane)

--- packages/sn-filter-pane/src/ext/property-panel/data/data-panel/presentation/styling-definitions/styling-panel-def.js
+++ packages/sn-filter-pane/src/ext/property-panel/data/data-panel/presentation/styling-definitions/styling-panel-def.js
@@ -1,16 +1,76 @@
 const stylingPanelDef = {
-    type: 'items',
-    items: [
-        {
-            component: 'styling-panel',
-            chartType: 'filterpane',
-            translation: 'LayerStyleEditor.component.styling',
-            chartTitle: 'Object.FilterPane',
-            useBackground: true,
-            subtitle: 'LayerStyleEditor.component.styling',
-            useGeneral: true,
-        }
-    ]
+    component: 'styling-panel',
+    chartType: 'filterpane',
+    translation: 'LayerStyleEditor.component.styling',
+    chartTitle: 'Object.FilterPane',
+    useBackground: true,
+    subtitle: 'LayerStyleEditor.component.styling',
+    useGeneral: true,
+    items: {
+        headerFontSection: {
+            component: 'panel-section',
+            translation: 'properties.Header',
+            items: {
+                headerFontItem: {
+                    component: 'items',
+                    ref: 'components',
+                    key: 'theme',
+                    items: {
+                        headerFontWrapper: {
+                            component: 'inline-wrapper',
+                            items: {
+                                headerFontItem: {
+                                    component: 'items',
+                                    ref: 'components',
+                                    key: 'theme',
+                                    items: {
+                                        headerFontWrapper: {
+                                            component: 'inline-wrapper',
+                                            items: {
+                                                headerFontSize: {
+                                                    component: 'integer',
+                                                    ref: 'title.main.fontSize',
+                                                    translation: 'properties.fontSize',
+                                                    width: 9,
+                                                    min: 5,
+                                                    max: 300,
+                                                    defaultValue(item, data, args) {
+                                                        const currentTheme = args.theme.current();
+                                                        // should we get the default values from listBox? (filterpane object from theme has just a backgroundColor property?)
+                                                        return parseInt(
+                                                            currentTheme.object?.listBox?.title?.main?.fontSize ?? currentTheme.fontSize,
+                                                            10
+                                                        );
+                                                    },
+                                                    change(data) {
+                                                        // TODO: values change not applying
+                                                        data.title.main.fontSize = !data.title.main.fontSize
+                                                            ? data.title.main.fontSize
+                                                            : Math.max(5, Math.min(300, Math.floor(data.title.main.fontSize)));
+                                                      },
+                                                },
+                                                headerFontColor: {
+                                                    show: true,
+                                                    ref: 'title.main.color',
+                                                    type: 'object',
+                                                    component: 'color-picker',
+                                                    defaultValue(item, data, args) {
+                                                        const currentTheme = args.theme.current();
+                                                        return { color: currentTheme.object?.listBox?.title?.main?.color ?? currentTheme.color };
+                                                    },
+                                                },
+                                            },
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+
+    },
 };
 
 
--- packages/sn-filter-pane/src/ext/property-panel/settings.ts
+++ packages/sn-filter-pane/src/ext/property-panel/settings.ts
@@ -1,23 +1,29 @@
+import { IEnv } from "../../types/types";
 import stylingPanelDef from "./data/data-panel/presentation/styling-definitions/styling-panel-def";
 
-const settings = {
-  uses: 'settings',
-  items: {
-    general: {
-      items: {
-        showTitles: {
-          defaultValue: false,
+function getSettings(env: IEnv) {
+  const { flags } = env || {};
+  const stylingPanelEnabled = flags.isEnabled('IM_4073_FILTERPANE_STYLING')
+  const stylingPanel = stylingPanelEnabled && stylingPanelDef;
+  return {
+    uses: 'settings',
+    items: {
+      general: {
+        items: {
+          showTitles: {
+            defaultValue: false,
+          },
         },
       },
+      presentation: {
+        grouped: true,
+        type: 'items',
+        translation: 'properties.presentation',
+        items: [stylingPanel]
+      },
+      simpleLabels: null,
     },
-    presentation: {
-      grouped: true,
-      type: 'items',
-      translation: 'properties.presentation',
-      items: [stylingPanelDef]
-    },
-    simpleLabels: null,
-  },
+  }
 };
 
-export default settings;
+export default getSettings;
