debe41ba1d4102065aff41e38d45f6e22834dee8
costa <costantino.marullo@qlik.com>
2023-08-22 15:13:55 +0200

    fix: add layout components to listbox resources

--- packages/sn-filter-pane/src/hooks/listbox/get-listbox-resources.ts
+++ packages/sn-filter-pane/src/hooks/listbox/get-listbox-resources.ts
@@ -1,8 +1,12 @@
 import { IFilterPaneLayout, ListboxResourcesArr } from '../types';
 
-async function getListboxResourcesFromIds(app: EngineAPI.IApp, ids: string[]): Promise<ListboxResourcesArr> {
+async function getListboxResourcesFromIds(app: EngineAPI.IApp, ids: string[], layout: IFilterPaneLayout): Promise<ListboxResourcesArr> {
   const models = await Promise.all(ids.map((id) => app.getObject(id)));
   const layouts = await Promise.all(models.map((model) => model.getLayout() as unknown as EngineAPI.IGenericListLayout));
+  // add components object for styling
+  if(layouts && layouts.length > 0) {
+    layouts[0].components = layout['components']
+  };
 
   return ids.map((id: string, index: number) => ({
     id,
@@ -13,5 +17,5 @@ async function getListboxResourcesFromIds(app: EngineAPI.IApp, ids: string[]): P
 
 export default async function getListBoxResources(app: EngineAPI.IApp, layout: IFilterPaneLayout): Promise<ListboxResourcesArr> {
   const childIds = layout?.qChildList?.qItems.map((item) => item.qInfo.qId) || [];
-  return getListboxResourcesFromIds(app, childIds);
+  return getListboxResourcesFromIds(app, childIds, layout);
 }
