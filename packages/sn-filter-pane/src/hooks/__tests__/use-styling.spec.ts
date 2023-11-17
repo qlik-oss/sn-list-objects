df187e381e46938dc68a0b260c084762136ab0af
Johan Lahti <johanlahti@gmail.com>
2023-11-17 12:40:21 +0100

    test: fix

--- packages/sn-filter-pane/src/hooks/__tests__/use-styling.spec.ts
+++ packages/sn-filter-pane/src/hooks/__tests__/use-styling.spec.ts
@@ -19,13 +19,14 @@ describe('use styling', () => {
     const components: IComponent[] = [];
     const styles = useStyling({ components });
     expect(styles).toMatchObject({
-      backgroundColor: 'object//filterpane.backgroundColor',
       listbox: {
-        backgroundColor: 'object.listBox//backgroundColor',
-        color: 'object.listBox/title.main/color',
+        background: {
+          backgroundColor: 'object.listBox//backgroundColor',
+          color: 'object.listBox//backgroundColor',
+        },
       },
       popover: {
-        backgroundColor: 'object//filterpane.backgroundColor',
+        backgroundColor: 'object.listBox//backgroundColor',
       },
       header: {
         color: 'object.listBox/title.main/color',
@@ -52,13 +53,17 @@ describe('use styling', () => {
     ];
     const styles = useStyling({ components });
     expect(styles).toMatchObject({
-      backgroundColor: 'object//filterpane.backgroundColor',
       listbox: {
-        backgroundColor: 'object.listBox//backgroundColor',
+        background: {
+          color: 'object.listBox//backgroundColor',
+          backgroundColor: 'object.listBox//backgroundColor',
+          backgroundRepeat: 'no-repeat',
+          opacity: 1,
+        },
         color: 'ComponentHeader',
       },
       popover: {
-        backgroundColor: 'object//filterpane.backgroundColor',
+        backgroundColor: 'object.listBox//backgroundColor',
       },
       header: {
         color: 'ComponentHeader',
@@ -66,6 +71,7 @@ describe('use styling', () => {
         fontFamily: 'object.listBox/title.main/fontFamily',
         fontWeight: 'object.listBox/title.main/fontWeight',
       },
+      stardustTheme: {},
     });
   });
 });
