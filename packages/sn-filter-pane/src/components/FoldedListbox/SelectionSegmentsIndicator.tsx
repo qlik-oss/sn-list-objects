807fc896882c68873359b57a6d361b8f860ae352
Johan Lahti <johanlahti@gmail.com>
2024-03-21 09:23:01 +0100

    refactor: use a styled component instead

--- packages/sn-filter-pane/src/components/FoldedListbox/SelectionSegmentsIndicator.tsx
+++ packages/sn-filter-pane/src/components/FoldedListbox/SelectionSegmentsIndicator.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid } from '@mui/material';
+import { Grid, styled } from '@mui/material';
 import getSegmentsSizes from './get-segment-sizes';
 import { ISelectionsComponent, IStyles } from '../../hooks/types/components';
 
@@ -10,6 +10,14 @@ interface SelectionSegmentsIndicatorProps {
 }
 
 type SelectionState = ('selected' | 'selectedExcluded' | 'possible' | 'alternative' | 'excluded');
+
+interface ISegmentProps {
+  segmentStyle: object;
+}
+
+const StyledSegment = styled(Grid, (({ shouldForwardProp: (p: string) => !['segmentStyle'].includes(p) })))(({ segmentStyle }: ISegmentProps) => ({
+  ...segmentStyle,
+}));
 
 const SelectionSegmentsIndicator = ({ qDimensionInfo, fullHeight, styles }: SelectionSegmentsIndicatorProps) => {
   if (!qDimensionInfo) {
@@ -33,7 +41,7 @@ const SelectionSegmentsIndicator = ({ qDimensionInfo, fullHeight, styles }: Sele
   return (
     <Grid container borderTop='1px solid #cccccc' height={fullHeight ? '100%' : undefined}>
       {segments.map((s) => (
-        <Grid item key={s.key} style={s.style} />
+        <StyledSegment item key={s.key} segmentStyle={s.style} />
       ))}
     </Grid>
   );
