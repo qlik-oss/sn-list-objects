bd4c83b97e37ffb0e0d3f6ea1ea8325e6f82ed84
linhnguyen-qlik <lgu@qlik.com>
2024-04-12 12:03:57 +0200

    refactor: github warning

--- packages/sn-filter-pane/src/components/FoldedListbox/DimensionIcon.tsx
+++ packages/sn-filter-pane/src/components/FoldedListbox/DimensionIcon.tsx
@@ -15,23 +15,25 @@ const DimensionIcon = ({
 }: Props) => {
   const delay = 2000;
   const style = { fontSize: '12px', padding: isRtl ? '0 0 0 6px' : '0 6px 0 0' };
+  let title;
+  let icon;
   switch (type) {
-    case 'H': return (
-        <Tooltip title={translator?.get('Listbox.DrillDown')} enterDelay={delay}>
-          <div>
-            <DrillDown style={style} />
-          </div>
-        </Tooltip>
-    );
-    case 'C': return (
-      <Tooltip title={translator?.get('Listbox.Cyclic')} enterDelay={delay}>
-      <div>
-        <Cyclic style={style} />
-      </div>
-    </Tooltip>);
+    case 'H':
+      icon = (<DrillDown style={style} />);
+      title = translator?.get('Listbox.DrillDown');
+      break;
+    case 'C':
+      icon = (<Cyclic style={style} />);
+      title = translator?.get('Listbox.Cyclic');
+      break;
     default:
       return undefined;
   }
+  return (<Tooltip title={title} enterDelay={delay}>
+      <div>
+        {icon}
+      </div>
+    </Tooltip>);
 };
 
 export default DimensionIcon;
