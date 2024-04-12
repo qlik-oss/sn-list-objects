4fe6a55fa194048c13ef20304f66c933f159c1a6
linhnguyen-qlik <111750285+linhnguyen-qlik@users.noreply.github.com>
2024-04-12 12:10:18 +0200

    fix: prevent memory leak error (#425)

--- packages/sn-filter-pane/src/hooks/use-render.ts
+++ packages/sn-filter-pane/src/hooks/use-render.ts
@@ -1,28 +1,33 @@
-import { useEffect, useElement } from '@nebula.js/stardust';
+import { useEffect, useElement, useState } from '@nebula.js/stardust';
 import type { IStores } from '../store';
 import useListBoxResources from './listbox/get-listbox-resources';
 import { IContainerElement } from './types';
 import { render, teardown } from '../components/root';
 
 export default function useRender(stores: IStores) {
+  const [mounted] = useState<{current: boolean}>({ current: true });
   const { store } = stores;
 
   const { app, fpLayout } = store.getState();
   const containerElement = <IContainerElement>useElement();
 
-  const { resourcesReady } = useListBoxResources(stores);
+  const isMounted = () => mounted.current;
+
+  const { resourcesReady } = useListBoxResources(stores, isMounted);
 
   // Trigger a re-render only when components have changed in the filterpane layout.
   // (Note that useEffect equality check is shallow and therefore requires a hash.)
   const componentsHash = (fpLayout?.components || []).sort().map((c: object) => JSON.stringify(c)).join(',');
 
   useEffect(() => {
+    mounted.current = true;
     if (!fpLayout || !app || !resourcesReady) {
       return undefined;
     }
     const root = render(containerElement, stores);
-    return (() => {
+    return () => {
+      mounted.current = false;
       teardown(root);
-    });
+    };
   }, [resourcesReady, componentsHash]);
 }
